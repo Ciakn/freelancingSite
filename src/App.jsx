0cad5f28c0b2c44289fcaf52dcaa018cf3746b56
ciavash <svsh.kian@gmail.com>
2024-07-12 12:45:55 -0700

    creating auth component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import { Route,Routes } from "react-router-dom";
+import Auth from "./components/Auth";
 
 const App = () => {
-  return <div>پروزه</div>;
+  return <div className="">
+    <Routes>
+      <Route path="" element={<Auth/> } />
+    </Routes>
+      
+  </div>;
 };
 
 export default App;
