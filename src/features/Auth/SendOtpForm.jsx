2406f2d900905f81794d94ce46d5e71f1a490b13
ciavash <svsh.kian@gmail.com>
2024-07-12 13:35:51 -0700

    developing the sendOtop feature

--- src/features/Auth/SendOtpForm.jsx
+++ src/features/Auth/SendOtpForm.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React, { useState } from "react";
 
 const SendOtpForm = () => {
+  const [phoneNumber, setPhoneNumbber] = useState("");
   return (
-    <div>SendOtpForm</div>
-  )
-}
+    <div>
+      <form className="space-y-5">
+        <div>
+          <label className="mb-1" htmlFor="phoneNumber">
+            {" "}
+            شماره مبایل
+          </label>
+          <input
+            id="phoneNumber"
+            type="text"
+            className="w-full py-3 px-4 rounded-xl text-secondary-900 border 
+            broder-gray-100 hover:border-primary-300 focus:shadow-primary-900 
+            transition-all duration-300 ease-in-out focus:border-primary-300"
+          />
+        </div>
+        <button className="py-1 px-4 font-bold  text-white w-full rounded-md bg-primary-900 transition-all duration-300 hover:bg-primary-800 shadow-md shadow-primary-300">ارسال کد تایید</button>
+      </form>
+    </div>
+  );
+};
 
-export default SendOtpForm
+export default SendOtpForm;
